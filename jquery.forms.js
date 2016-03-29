848fada36c5077c8ad6f412124ffb636c5077216
Joey Caughey <joey.caughey@gmail.com>
2016-03-29 15:27:31 -0400

    Update jquery.forms.js

--- jquery.forms.js
+++ jquery.forms.js
@@ -1,45 +1,85 @@
+var Forms = {
+    validate: function(form) {
+        var self = this;
+        elements = form.elements;
 
-function validate(form) {
-    elements = form.elements;
-    checked = [];
+        $(form).find("div.errors").html("");
 
-    for(key = 0; key < elements.length; key++) {
-        valid = false;
-        name = elements[key].getAttribute("name");
-        value = elements[key].value;
-        required = elements[key].getAttribute("data-required");
+        if (!self.check(elements)) {
+            // Coment this out to disable popup.
+            //console.log(form, $(form).find("div.errors"))
+            if ($(form).find("div.errors")) {
+                $(form).find("div.errors")
+                    .html('Your form has erros and they are <span class="error">marked in red</span>')
+                    .show();    
 
-        //console.log(name, elements[key].tagName.toLowerCase(), "Req: ", required, "Value: ", value);
-
-        if (required) {
-            elements[key].className.replace(/\berror\b/,'');
-
-            required = required.toLowerCase();
-
-            if (required === "true" || required === "valid_isnotnull"  || required === "valid_notnull") {
-                valid = validation.valid(value);
+                $("html,body").animate({
+                    scrollTop: 0
+                }, 1000);           
             } else {
-                valid = validation[required](value);
-            }
-
-            checked.push(valid);
-
-            if (!valid) {
-                elements[key].classList.add("error");    
-                //elements[key].placeholder = elements[key].name + " is a required field";
-            } else {
-                elements[key].classList.remove("error"); 
+                Popup.init({
+                    width: 320,
+                    height: 80,
+                    content: '<h2>Your form has errors</h2><p>Your form has erros and they are <span class="error">marked in red</span>. Update them and try again.</p>'
+                });
+                Loading.hide();
             }
         }
+
+        return self.check(elements);
+    },
+    elements_are_valid: function(elements) {
+        var self = this;
+        return self.check(elements);
+    },
+    check: function(elements) {
+        checked = [];
+
+        //console.log(elements);
+
+        $.each(elements, function(key, element) {
+            valid = false;
+            name = element.getAttribute("name");
+            value = element.value;
+            required = element.getAttribute("data-required");
+
+            if (required) {
+                elements[key].className.replace(/\berror\b/, '');
+
+                required = required.toLowerCase();
+
+                //console.log(required);
+
+                if (required === "true" || required === "valid_isnotnull" || required === "valid_notnull") {
+                    valid = validation.valid(value);
+                } else {
+                    valid = validation[required](value);
+                }
+
+                checked.push(valid);
+
+                if (!valid) {
+                    element.classList.add("error");
+                    element.classList.remove("valid");
+
+                    $(element).parent().find("small").addClass("error")
+                        //elements[key].placeholder = elements[key].name + " is a required field";
+                } else {
+                    element.classList.remove("error");
+                    element.classList.add("valid");
+
+                    $(element).parent().find("small").removeClass("error")
+                }
+            }
+        });
+
+        return checked.contains(false) ? false : true;
     }
-
-    return checked.contains(false) ? false : true;
 }
-
 
 var validation = {
     valid: function(input) {
-        return (input!=null && input !="");
+        return (input != null && input != "");
     },
     valid_notnull: function(input) {
         return this.valid(input);
@@ -61,35 +101,83 @@ var validation = {
         var digits = p.replace(/\D/g, "");
         return regex.test(phone);
     },
-    valid_username: function(){
+    valid_username: function() {
         return true;
     },
-    valid_password: function() {
-        return true;
+    valid_password: function(input) {
+        var regex = new RegExp(/^(?=.*\d)(?=.*[A-Za-z])[0-9A-Za-z!@#$%]{8,20}$/);
+        if (regex.test(input))
+            return true;
+        else return false;
     },
-    valid_password_confirm: function(){
+    valid_password_confirm: function(input) {
+        var error = false;
+
+        if (input === "") {
+            error = "<br />Your new password can not be blank.";
+        }
+
+        if (input !== $("INPUT[name=password]").val()) {
+            error = "<br />Your new pasword and confirmation does not match.";
+        }
+
+        if (!validation.valid_password(input)) {
+            error = "<br />Your password must be at least 8 characters and be alphanumberic."
+        }
+
+        if (error) {
+            $("div.errors").append(error);
+
+            $("INPUT[name=passwordagain]").addClass("error");
+            return false;
+        }
+        $("INPUT[name=passwordagain]").removeClass("error");
         return true;
+
+
     },
-    valid_postal_code: function(input){
+    valid_new_password_confirm: function(input) {
+        var error = false;
+
+        if (input === "") {
+            error = "<br />Your new password can not be blank.";
+        }
+
+        if (input !== $("INPUT[name=password_new]").val()) {
+            error = "<br />Your new pasword and confirmation does not match.";
+        }
+
+        if (!validation.valid_password(input)) {
+            error = "<br />Your password must be at least 8 characters and be alphanumberic."
+        }
+
+        if (error) {
+            $("div.errors").append(error);
+
+            $("INPUT[name=passwordagain]").addClass("error");
+            return false;
+        }
+        $("INPUT[name=passwordagain]").removeClass("error");
+        return true;
+
+
+    },
+    valid_postal_code: function(input) {
         var regex = new RegExp(/^[a-zA-Z]\d[a-zA-Z]( )?\d[a-zA-Z]\d$/i);
         if (regex.test(input))
             return true;
         else return false;
     },
-    valid_location: function(input){
+    valid_location: function(input) {
         var self = this;
 
         if (!$("INPUT[name=anywhere]").is(":checked")) {
-            if (
-                !self.valid_notnull($("INPUT[name=intersection]").val())
-            ) {
+            if (!self.valid_notnull($("INPUT[name=intersection]").val())) {
                 $("INPUT[name=intersection]").addClass("error");
                 return false;
             }
 
-            if (
-                !self.valid_postal_code($("INPUT[name=zip_postal_code]").val())
-            ) {
+            if (!self.valid_postal_code($("INPUT[name=zip_postal_code]").val())) {
                 $("INPUT[name=zip_postal_code]").addClass("error");
                 return false;
             }
@@ -100,6 +188,49 @@ var validation = {
         if (ReCaptcha.length === 1 && ReCaptcha.is_valid()) return true;
         //alert("Captcha invalid.")
         return true;
+    },
+    valid_terms_of_service: function(input) {
+        if ($("INPUT[name=terms_of_service]").is(":checked")) {
+            $("INPUT[name=terms_of_service]").parent().removeClass("error");
+            return true;
+        }
+        $("INPUT[name=terms_of_service]").parent().addClass("error");
+        return false;
+    },
+
+    valid_price: function(price) {
+        if (price > 5 && price < 999) {
+            return true;
+        }
+        return false;
+    },
+    valid_regularprice: function(price) {
+        if (!$("INPUT[name=regular_price]").closest("fieldset").is(":visible") || validation.valid_price(price)) {
+            return true;
+        }
+        return false;
+    },
+    valid_standardprice: function(price) {
+        if (!$("INPUT[name=bl_standard_price]").closest("fieldset").is(":visible") ||
+            (validation.valid_price(price) && price < ($("INPUT[name=regular_price]").val() * 0.95))
+        ) {
+            return true;
+        }
+        return false;
+    },
+    valid_promoprice: function(price) {
+        if (!$("INPUT[name=bl_promo_price]").closest("fieldset").is(":visible") ||
+            (validation.valid_price(price) && price < ($("INPUT[name=regular_price]").val() * 0.90))
+        ) {
+            return true;
+        }
+        return false;
+    },
+    valid_groupdealprice: function(price) {
+        if (!$("INPUT[name=bl_group_deal_price]").closest("fieldset").is(":visible") || validation.valid_price(price)) {
+            return true;
+        }
+        return false;
     }
 }
 
@@ -117,33 +248,33 @@ function hasClass(element, cls) {
     return (' ' + element.className + ' ').indexOf(' ' + cls + ' ') > -1;
 }
 
-jQuery('form.standard input').keypress(function(e){
-    if (e.which == 13 ) return false;
-    if (e.which == 13 ) e.preventDefault();
-});
-
 function load_form_features() {
-	$("select[data-json]").each(function(){
+    $("select[data-json]").each(function() {
         var self = $(this);
         var params = $(this).data("json").split("|");
         var value_key = params[1];
         var name_key = params[2];
 
-        $.getJSON("_assets/json/"+params[0], function(timezones) {
-            $.each(timezones, function(j, data) {
+        $(this).removeAttr("data-json");
+
+        $.getJSON("/_assets/json/" + params[0], function(json_data) {
+            $.each(json_data, function(j, data) {
                 self.append(
                     $("<option>").html(data[name_key])
-                        .val(data[value_key])
+                    .val(data[value_key])
                 );
             })
         })
     });
 }
 
-console.log("Loaded form validation.");
+$(document).ready(function() {
+    $("INPUT, TEXTAREA, SELECT").on("focus", function() {
+        $(this).removeClass("error")
+    });
+});
 
-var css = document.createElement("link")
-css.setAttribute("rel", "stylesheet")
-css.setAttribute("type", "text/css")
-css.setAttribute("href", "/_assets/jquery.forms/jquery.forms.css");
-document.getElementsByTagName("head")[0].appendChild(css);
+$('form input').keypress(function(e) {
+    if (e.which == 13) return false;
+    if (e.which == 13) e.preventDefault();
+});
